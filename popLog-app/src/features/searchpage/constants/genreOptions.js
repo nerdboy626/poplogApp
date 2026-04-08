aea5880b2a01c8d4b43303d6a9a12f48bd8ca4c4
Nico Caminero <nerdboy0428@gmail.com>
2026-04-07 22:54:45 -0400

    fix(client): update NYT book genres

--- popLog-app/src/features/searchpage/constants/genreOptions.js
+++ popLog-app/src/features/searchpage/constants/genreOptions.js
@@ -17,14 +17,13 @@ const genreOptions = {
       id: "combined-print-and-e-book-fiction",
       name: "Combined Print & E‑Book Fiction",
     },
+    { id: "hardcover-fiction", name: "Hardcover Fiction" },
+    { id: "trade-fiction-paperback", name: "Paperback Trade Fiction" },
     {
       id: "combined-print-and-e-book-nonfiction",
       name: "Combined Print & E‑Book Nonfiction",
     },
-    { id: "hardcover-fiction", name: "Hardcover Fiction" },
     { id: "hardcover-nonfiction", name: "Hardcover Nonfiction" },
-    { id: "trade-fiction-paperback", name: "Paperback Trade Fiction" },
-    { id: "paperback-nonfiction", name: "Paperback Nonfiction" },
     {
       id: "advice-how-to-and-miscellaneous",
       name: "Advice, How-To & Miscellaneous",
@@ -36,15 +35,20 @@ const genreOptions = {
     { id: "picture-books", name: "Children’s Picture Books" },
     { id: "series-books", name: "Children’s & Young Adult Series" },
     { id: "young-adult-hardcover", name: "Young Adult Hardcover" },
+    {
+      id: "audio-advice-how-to-and-miscellaneous",
+      name: "Audio Advice, How-To & Misc",
+    },
+    { id: "audio-childrens", name: "Audio Children's" },
     { id: "audio-fiction", name: "Audio Fiction" },
     { id: "audio-nonfiction", name: "Audio NonFiction" },
     { id: "business-books", name: "Business" },
     { id: "graphic-books-and-manga", name: "Graphic Books and Manga" },
-    { id: "mass-market-monthly", name: "Mass Market" },
     {
       id: "middle-grade-paperback-monthly",
       name: "Middle Grade Paperback",
     },
+    { id: "paperback-nonfiction-monthly", name: "Paperback Nonfiction" },
     {
       id: "young-adult-paperback-monthly",
       name: "Young Adult Paperback",
