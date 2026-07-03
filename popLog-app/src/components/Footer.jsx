72769cf227fd254d9abf3e60bf52602b77564a97
Nico Caminero <nerdboy0428@gmail.com>
2026-07-02 22:11:34 -0400

    feat: add a footer to website

---
+++ popLog-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__inner">
+        <p>© 2026 Nicolas Caminero</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- popLog-app/src/layouts/RootLayout.jsx
+++ popLog-app/src/layouts/RootLayout.jsx
@@ -1,16 +1,23 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import Loading from "../components/Loading.jsx";
 import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 import AuthRedirect from "../components/AuthRedirect.jsx";
+import "./RootLayout.css";
 
 const RootLayout = () => {
   const navigation = useNavigation();
   return (
-    <>
+    <div className="layout">
       <AuthRedirect />
       <Navbar />
-      {navigation.state === "loading" ? <Loading /> : <Outlet />}
-    </>
+
+      <main className="layout__content">
+        {navigation.state === "loading" ? <Loading /> : <Outlet />}
+      </main>
+
+      <Footer />
+    </div>
   );
 };
 
